ed6ab220687d06e983c6a2c81bd330b95d9dd5ac
huythanh <huythanh1706@gmail.com>
2020-02-04 08:30:19 +0700

    Chapter 4: Rails-flavored Ruby:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title page_title = ""
+    base_title = t "static_pages.home.tutorial"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
